72f70ba6be20154894cb0814f9e6bbbcd3cbd353
jstable1 <jessica.stabler@gmail.com>
2022-03-03 11:03:59 -0700

    created passwordPrompts function

--- Develop/script.js
+++ Develop/script.js
@@ -1,4 +1,48 @@
 // Assignment code here
+var specialCharactersArray = ["!", "?", "@", "$"];
+var numberChoiceArray = ["1", "2", "3", "4"];
+var lowerCaseChoiceArray = ["a", "b", "c", "d"];
+var upperCaseArray = ["A", "B", "C", "D"];
+var userChoices = [];
+
+var passwordPrompts = function(){
+  var promptLength = window.prompt('The password must be between 8-128 characters. How long would you like the password to be?');
+
+  if (promptLength === "" || promptLength === null || promptLength < 8 || promptLength > 128) {
+    window.alert("You need to provide a valid answer! Please try again.");
+    return;
+  }
+
+  var specialChoice = window.confirm("Would you like to include special characters?");
+    if (specialChoice === true) {
+      userChoices = userChoices.concat(specialCharactersArray);
+    }
+
+  var numberChoice = window.confirm("Would you like to include numbers?");
+    if (numberChoice === true) {
+      userChoices = userChoices.concat(numberChoice);
+    }
+
+  var lowerCaseChoice = window.confirm("Would you like to include lower case?");
+    if (lowerCaseChoice === true) {
+      userChoices = userChoices.concat(lowerCaseChoice);
+    }
+
+  var upperCaseChoice = window.confirm("Would you like to include upper case?");
+    if (upperCaseChoice === true) {
+      userChoices = userChoices.concat(upperCaseChoice);
+    }
+
+  if (specialChoice === false && numberChoice === false && lowerCaseChoice === false && upperCaseChoice === false) {
+    window.alert("You need to choose at least 1 character type");
+    return;
+  }
+}
+
+
+var generatePassword = function(){
+  passwordPrompts();
+}
 
 
 // Get references to the #generate element
