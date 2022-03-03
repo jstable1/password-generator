4f2d55c81d82a1b472faced4fe018795949f5f09
jstable1 <jessica.stabler@gmail.com>
2022-03-03 11:10:18 -0700

    finished creating password choices arrays

--- Develop/script.js
+++ Develop/script.js
@@ -1,8 +1,8 @@
 // Assignment code here
-var specialCharactersArray = ["!", "?", "@", "$"];
-var numberChoiceArray = ["1", "2", "3", "4"];
-var lowerCaseChoiceArray = ["a", "b", "c", "d"];
-var upperCaseArray = ["A", "B", "C", "D"];
+var specialCharactersArray = ["!", "?", "@", "$", "#", "%", "*"];
+var numberChoiceArray = ["1", "2", "3", "4", "5", "6", "7", "8", "9"];
+var lowerCaseChoiceArray = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
+var upperCaseArray = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
 var userChoices = [];
 
 var passwordPrompts = function(){
